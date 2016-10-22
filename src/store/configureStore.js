96ea12b0a4b53137892ffd0c0705257191b756ef
vikram-jakhar <vikram.jakhr@gmail.com>
2016-10-22 16:43:47 +0530

    commit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,16 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reducers from "../reducers/index";
+import thunk from "redux-thunk";
+import createLogger from "redux-logger";
 
-let store = createStore(reducers);
+let middleware = [thunk];
+const logger = createLogger();
+middleware.push(createLogger);
+
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk, logger)
+);
+
 export default store;
-
-store.dispatch({type:"HELLO"});
